9fcf2b34d2354454e0dd3fdb435e0f3479bc2239
simon-gilmour-main <simon.j.gilmour.main@gmail.com>
2026-04-17 11:50:11 +0100

    Jenkinsfile branch (#1)
    * Adding Jenkinsfile for automation
    
    * update 2
    
    * update 3

--- infra-demo/terraform/dev.tfvars
+++
@@ -1,3 +0,0 @@
-aws_region    = "eu-west-2"
-instance_type = "t3.micro"
-instance_name = "terraform-puppet-demo"
--- infra-demo/terraform/variables.tf
+++ infra-demo/terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "allowed_cidr" {
+  type        = string
+  description = "Your public IP in CIDR form, e.g. 1.2.3.4/32"
+}
+
 variable "aws_region" {
   type    = string
   default = "eu-west-2"
